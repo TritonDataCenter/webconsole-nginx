eeda7e779ec8ca3e3d830f832ed91895b7bcd0a9
geek <wpreul@gmail.com>
2018-04-25 09:37:58 -0500

    Add accounts data file

---
+++ data/accounts.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    name: 'Account',
+    slug: 'account',
+    description: '',
+    url: 'https://my.joyent.com/main/#!/account'
+  },
+  {
+    name: 'Change Password',
+    slug: 'change-password',
+    description: 'Change your account password'
+  },
+  {
+    name: 'Two Factor Authentication',
+    slug: '2fa',
+    description: ''
+  },
+  {
+    name: 'Log Out',
+    slug: 'logout',
+    description: 'Sign out of your session',
+    url: '/navigation/logout'
+  }
+]
